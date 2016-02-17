2d61837faf8e9fe9dd91f7a793169cbd1b3bfd3b
vishal kulkarni <vishal kulkarni>
2016-02-18 00:02:44 +0530

    Version 1.2 : User gets a messge after winning the game

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ Version 1.1
 
 Added some more features
 
+Version 1.2
+-----------
+
+Most stable version with all feature working properly.
++ You will get a message after winning the game
--- src/com/games/minesweeper/Minesweeper.java
+++ src/com/games/minesweeper/Minesweeper.java
@@ -3,12 +3,11 @@ package com.games.minesweeper;
 import java.util.Scanner;
 
 public class Minesweeper {
-	
 
 	public static void main(String[] args) {
 		int x = 10;
 		int y = 10;
-		double z = 0.1;
+		double z = 0.05;
 
 		boolean[][] bomb = new boolean[x + 2][y + 2];
 
@@ -25,25 +24,24 @@ public class Minesweeper {
 		 */
 
 		// live problem display
-		
-		/*for (int i = 1; i <= (x); i++) {
-			for (int j = 1; j <= (y); j++) {
-				if (bomb[i][j])
-					System.out.print("@ ");
-				else
-					System.out.print(". ");
-			}
-			System.out.println();
 
-		}*/
-		
-		
+		/*
+		 * for (int i = 1; i <= (x); i++) { for (int j = 1; j <= (y); j++) { if
+		 * (bomb[i][j]) System.out.print("@ "); else System.out.print(". "); }
+		 * System.out.println();
+		 * 
+		 * }
+		 */
+		int BombCounter = 0;
+
 		int sol[][] = new int[x + 2][y + 2];
-		
+
 		for (int i = 1; i <= (x); i++) {
 			for (int j = 1; j <= (y); j++) {
-				if (bomb[i][j])
+				if (bomb[i][j]) {
 					sol[i][j] = -9; // marking all the bombs as - integer
+					BombCounter++;
+				}
 				for (int ii = i - 1; ii <= i + 1; ii++) {
 					for (int jj = j - 1; jj <= j + 1; jj++) {
 						if (bomb[ii][jj])
@@ -53,8 +51,6 @@ public class Minesweeper {
 			}
 
 		}
-		
-		
 
 		// displaying sol[][]
 		// for (int i = 1; i < sol.length-1; i++) {
@@ -70,155 +66,184 @@ public class Minesweeper {
 		boolean flag = true;
 
 		int disp[][] = new int[x + 2][y + 2];
-		
-		
-		
-		for (int i1 = 1; i1 <= x+1; i1++) {
-			for (int j1 = 1; j1 <= y+1; j1++) {
-				
+
+		for (int i1 = 1; i1 <= x + 1; i1++) {
+			for (int j1 = 1; j1 <= y + 1; j1++) {
+
 				if (j1 > y && i1 != j1)
-					System.out.print(" :" + i1);
+					System.out.print(":" + i1);
 				else if (i1 > x && i1 != j1)
 					System.out.print(j1 + " ");
 				else if (i1 > x && j1 > y && i1 == j1)
 					System.out.println(" ");
-				 else System.out.print("■ ");
+				else
+					System.out.print("■ ");
 			}
 			System.out.println();
 		}
-		
-		
-		
 
-		
+		int BlockCounter = 0;
+
 		while (flag) {
 			int xx = 0;
 			int yy = 0;
-
+			BlockCounter = 0;
 			// getting x and y coordinates from user
 
 			Scanner in = new Scanner(System.in);
-			System.out.println("Enter the block's X co-ordinate ");
+			System.out.println("Enter the X co-ordinate ");
 			xx = in.nextInt();
-			System.out.println("Enter the block's Y co-ordinate ");
+			System.out.println("Enter the Y co-ordinate ");
 			yy = in.nextInt();
 
-			// display board as per users input
+			if (xx <= x && yy <= y && xx > 0 && yy > 0) {
+				// display board as per users input
 
-			for (int k = 0; k < 4; k++) {
-				
-				int i = xx;
-				int j = yy;
-				stop: while (i < 11 && i > 0) {
-					
-						
-					while (j < 11 && j >0) {
-						
-						if((i>=1 && i<=10) && (j>=1 && j<=10)){
-						if (bomb[i][j]) {
-							disp[i][j] = -1;
-							flag = false;
-							break stop;
+				for (int k = 0; k < 4; k++) {
 
-						} 
-						else if (sol[i][j] > 0) {
-							disp[i][j] = sol[i][j];
-							//flag = false;
-							if(j==yy)
-							{ 
-								break stop;
+					int i = xx;
+					int j = yy;
+					stop: while (i < 11 && i > 0) {
+
+						while (j < 11 && j > 0) {
+
+							if ((i >= 1 && i <= 10) && (j >= 1 && j <= 10)) {
+								if (bomb[i][j]) {
+									disp[i][j] = -1;
+									flag = false;
+									break stop;
+
+								} else if (sol[i][j] > 0) {
+									disp[i][j] = sol[i][j];
+									// flag = false;
+									if (j == yy) {
+										break stop;
+									}
+									break;
+
+								} else if (sol[i][j] == 0) {
+									disp[i][j] = -2;
+								}
+
 							}
-							break;
-							
-						} else if (sol[i][j] == 0) {
-							disp[i][j] = -2;
+							if (k == 0 || k == 1 && (j <= 10 && j >= 1)) {
+								j++;
+							} else if (k == 2 || k == 3 && (j <= 10 && j >= 1)) {
+								j--;
+							}
 						}
-						
-					}
-						if (k == 0 || k == 1 && (j<=10 && j>=1)) {
-							j++;
-						} else if (k == 2 || k == 3 && (j<=10 && j>=1)) {
-							j--;
+						j = yy;
+						if (k == 0 || k == 3 && (i <= 10 && i >= 1)) {
+							i++;
+						} else if (k == 1 || k == 2 && (i <= 10 && i >= 1)) {
+							i--;
 						}
 					}
-					j = yy;
-					if (k == 0 || k == 3 && (i<=10 && i>=1)) {
-						i++;
-					} else if (k == 1 || k == 2 && (i<=10 && i>=1)) {
-						i--;
-					}
+
+					/*
+					 * System.out.println("Value of K =" + k); for (int i1 = 1;
+					 * i1 < disp.length - 1; i1++) { for (int j1 = 1; j1 <
+					 * disp.length - 1; j1++) { if (disp[i1][j1] < 0)
+					 * System.out.print("@" + " "); else
+					 * System.out.print(disp[i1][j1] + " "); }
+					 * System.out.println(); }
+					 */
+
 				}
-				
-/*				System.out.println("Value of K =" + k);
-				for (int i1 = 1; i1 < disp.length - 1; i1++) {
-					for (int j1 = 1; j1 < disp.length - 1; j1++) {
-						if (disp[i1][j1] < 0)
-							System.out.print("@" + " ");
+
+				// displaying disp[][]
+				System.out.println("OUTPUT:");
+
+				for (int i1 = 1; i1 <= x + 1; i1++) {
+					for (int j1 = 1; j1 <= y + 1; j1++) {
+
+						if (j1 > y && i1 != j1)
+							System.out.print(":" + i1);
+						else if (i1 > x && i1 != j1)
+							System.out.print(j1 + " ");
+						else if (i1 > x && j1 > y && i1 == j1)
+							System.out.println(" ");
 						else
+
+						if (disp[i1][j1] == -1)
+							System.out.print("@" + " ");
+						else if (disp[i1][j1] > 0)
 							System.out.print(disp[i1][j1] + " ");
+						else if (disp[i1][j1] == -2)
+							System.out.print("□ ");
+						else {
+							BlockCounter++;
+							System.out.print("■ ");
+							
+						}
 					}
+					
+					
 					System.out.println();
-				}*/
+				}
+				if (BombCounter == BlockCounter) {
+					flag = false;					
+				}
+				System.out.println("-------------------------------");
 
-			}
+			} else
+				System.out.println("Try again with proper values..");
+			// System.out.println();
 
-			// displaying disp[][]
-			System.out.println("OUTPUT:");
+		}
 
-			for (int i1 = 1; i1 <= x+1; i1++) {
-				for (int j1 = 1; j1 <= y+1; j1++) {
-					
-					if (j1 > y && i1 != j1)
-						System.out.print(" :" + i1);
-					else if (i1 > x && i1 != j1)
-						System.out.print(j1 + " ");
-					else if (i1 > x && j1 > y && i1 == j1)
+		if (flag == false && (BombCounter != BlockCounter)) {
+			System.out.println("///////////GAME OVER///////////");
+			// -----------
+			System.out.println("-------------------------------");
+
+			System.out.println();
+			System.out.println("Solution Matrix");
+			System.out.println();
+			for (int i = 1; i <= (x + 1); i++) {
+				for (int j = 1; j <= (y + 1); j++) {
+					if (bomb[i][j])
+						System.out.print("@ ");
+					else if (j > y && i != j)
+						System.out.print(" :" + i);
+					else if (i > x && i != j)
+						System.out.print(j + " ");
+					else if (i > x && j > y && i == j)
 						System.out.println(" ");
-					 else
-					
-					if (disp[i1][j1] == -1)
-						System.out.print("@" + " ");
-					else if (disp[i1][j1] >0)
-						System.out.print(disp[i1][j1] + " ");
-					else if (disp[i1][j1]== -2)
-						System.out.print("□ ");
-					else System.out.print("■ ");
+					else if (sol[i][j] == 0) {
+						System.out.print(". ");
+					} else
+						System.out.print(sol[i][j] + " ");
 				}
 				System.out.println();
-			}
-			System.out.println("--------------------------------");
-
-		}
-		
-		System.out.println("///////////GAME OVER///////////");
-		// -----------
-		System.out.println("--------------------------------");
-		
-		System.out.println();
-		System.out.println("Solution Matrix");
-		System.out.println();
-		for (int i = 1; i <= (x + 1); i++) {
-			for (int j = 1; j <= (y + 1); j++) {
-				if (bomb[i][j])
-					System.out.print("@ ");
-				else if (j > y && i != j)
-					System.out.print(" :" + i);
-				else if (i > x && i != j)
-					System.out.print(j + " ");
-				else if (i > x && j > y && i == j)
-					System.out.println(" ");
-				else if (sol[i][j] == 0) {
-					System.out.print(". ");
-				} else
-					System.out.print(sol[i][j] + " ");
 
 			}
+		} else if (BombCounter == BlockCounter) {
+			System.out.println("----------------------------------");
+			System.out.println("//////CONGRATULATION YOU WON//////");
+			System.out.println("----------------------------------");
+			System.out.println("Solution Matrix");
 			System.out.println();
+			for (int i = 1; i <= (x + 1); i++) {
+				for (int j = 1; j <= (y + 1); j++) {
+					if (bomb[i][j])
+						System.out.print("@ ");
+					else if (j > y && i != j)
+						System.out.print(" :" + i);
+					else if (i > x && i != j)
+						System.out.print(j + " ");
+					else if (i > x && j > y && i == j)
+						System.out.println(" ");
+					else if (sol[i][j] == 0) {
+						System.out.print(". ");
+					} else
+						System.out.print(sol[i][j] + " ");
+				}
+				System.out.println();
 
+			}
 		}
-		
 
 	}
 
 }
-
