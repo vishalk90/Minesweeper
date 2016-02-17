f9b1ad14597412dba6afdd0707767a9be5ea9de7
Vishal Kulkarni <vishal.kulkarni1990@gmail.com>
2016-02-17 08:37:31 +0530

    modification done in the code to display solution array

--- src/com/games/minesweeper/Minesweeper.java
+++ src/com/games/minesweeper/Minesweeper.java
@@ -1,6 +1,9 @@
-package com.minesweeper.game;
+package com.games.minesweeper;
+
+import java.util.Scanner;
 
 public class Minesweeper {
+
 
 	
 	public static void main(String[] args) {
@@ -37,6 +40,7 @@ public class Minesweeper {
  	int sol[][] = new int[x+2][y+2];
 		for (int i = 1; i <= (x); i++) {
 			for (int j = 1; j <= (y); j++) {
+				if(bomb[i][j]) sol[i][j]=-9; // marking all the bombs as - integer
 				for(int ii = i-1; ii <=i+1; ii++){
 					for (int jj = j-1; jj <= j+1; jj++) {
 						if(bomb[ii][jj] ) sol[i][j]++;
@@ -59,8 +63,54 @@ public class Minesweeper {
 			System.out.println(	);
 		
  		}
+ 		// displaying sol[][]
+ 		for (int i = 1; i < sol.length-1; i++) {
+			for (int j = 1; j < sol.length-1; j++) {
+				if (sol[i][j]<0)System.out.print(sol[i][j]);
+				else System.out.print(" " + sol[i][j] );
+			}
+			System.out.println();
+		}
+ 		
+ 		/*
+ 		boolean flag = true;
+ 		int xx;
+ 		int yy;
+ 	 	int disp[][] = new int[x+2][y+2];
+ 		while(flag)
+ 		{
+ 			// getting x and y coordinates from user
+ 	 		Scanner in = new Scanner(System.in);
+ 	 		System.out.println("Enter the block's X co-ordinate ");
+ 	 		xx = in.nextInt();
+ 	 		System.out.println("Enter the block's Y co-ordinate ");
+ 	 		yy = in.nextInt();
+ 	 		
+ 	 		// display board as per users input
+ 	 		
+ 	 		for(int k =0; k<4; k++)
+ 	 		{
+ 	 			int i=xx+1;
+ 	 			stop: while(i<11)
+ 	 			{
+ 	 				int j = yy+1;
+ 	 				while(j<11)
+ 	 				{
+ 	 					if(sol[i][j]=='@')
+ 	 					{
+ 	 						
+ 	 					}
+ 	 				}
+ 	 			}
+ 	 		}
+ 	 		
+ 		}
  	
- 	
+ 		
+ 		
+ 		*/
+ 		
+ 		
 	}
 
 }
